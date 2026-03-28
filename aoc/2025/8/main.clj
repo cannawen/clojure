6e52a00d4d9214e3ccdda68bbd5c5267552d3021
Canna Wen <cannawen@gmail.com>
2026-03-27 21:30:08 -0400

    Pt 2 https://adventofcode.com/2025/day/8
    Co-Authored-By: Rafal Dittwald <rafal.dittwald@gmail.com>

--- aoc/2025/8/main.clj
+++ aoc/2025/8/main.clj
@@ -1,7 +1,35 @@
 (ns aoc.2025.8.main
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :as string]
+            [clojure.set :as set]))
 
-(->> "aoc/2025/8/input-mini.txt"
-     slurp
-     string/split-lines
-     (map (fn [s] (map parse-long (string/split s #"," )))))
+(defn distance [[v1 v2]]
+  (apply + (map (fn [a b] (* (- a b) (- a b))) v1 v2)))
+
+(let [points (->> "aoc/2025/8/input-mini.txt"
+                  slurp
+                  string/split-lines
+                  (map (fn [s] (mapv parse-long (string/split s #",")))))
+      connections (->> (for [a points
+                             b points
+                             :when (< 0 (compare a b))]
+                         [a b])
+                       (sort-by distance)
+                       (map set))
+      initial-circuits (map (fn [p] #{p}) points)]
+  (->> connections
+       (reductions (fn [memo connection]
+                     ;; find all circuits in memo that overlap with connection and remove them from memo
+                     ;; combine connection with the removed circuits and add back to memo
+                     (let [{others true
+                            overlapping false}
+                           (group-by (fn [circuit] (empty? (set/intersection circuit connection))) memo)]
+                       (with-meta
+                         (conj others (apply set/union overlapping))
+                         {:connection connection}))) 
+                   initial-circuits)
+       (filter (fn [circuits] (= 1 (count circuits))))
+       first
+       meta
+       :connection
+       (map first)
+       (apply *)))
